9944ab410f6c9f74025a2964c306041ceaab12b0
ClintFix <clint.fix@gmail.com>
2021-03-23 13:45:04 -0600

    set up local state management for login form

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,18 +1,32 @@
-import e from 'cors'
-import React from 'react'
+
+import React, {useState} from 'react'
+
+const initialFormValues = {
+    username: '',
+    password: ''
+}
 
 const Login = () => {
+    const [formValues, setFormValues] = useState(initialFormValues)
+
+    const handleChange = e => {
+        setFormValues({
+            ...formValues,
+            [e.target.name]: e.target.value,
+        })
+    }
 
     const submit = (e) => {
         e.preventDefault();
         //TODO
+        setFormValues(initialFormValues)
     }
 
     return (
         <div>
             <form onSubmit={submit}>
-                <input type='text' placeholder='Username'/>
-                <input type='password' placeholder='Password'/>
+                <input name={'username'} type='text' placeholder='Username' value={formValues.username} onChange={handleChange}/>
+                <input name={'password'} type='password' placeholder='Password' value={formValues.password} onChange={handleChange}/>
                 <button>Login</button>
             </form>
         </div>
