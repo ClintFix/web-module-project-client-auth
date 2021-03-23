4ca6cff732bcb7d66e633852cbc36365a5eb48d3
ClintFix <clint.fix@gmail.com>
2021-03-23 13:37:05 -0600

    created new login form

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,10 +1,20 @@
+import e from 'cors'
 import React from 'react'
 
 const Login = () => {
 
+    const submit = (e) => {
+        e.preventDefault();
+        //TODO
+    }
+
     return (
         <div>
-
+            <form onSubmit={submit}>
+                <input type='text' placeholder='Username'/>
+                <input type='password' placeholder='Password'/>
+                <button>Login</button>
+            </form>
         </div>
     )
 }
